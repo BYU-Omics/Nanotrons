7e0f8f2de7eb747c48ea44aafd966df42c19eadc
alebrozalez <alejandrobrozalezv@gmail.com>
2022-02-07 15:58:12 -0700

    (Yhann Masbernat/Alejandro Brozalez) Added the offcicial wix labware page that contains all the fuctionality of the sublinks in previous versions all in one page.

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 RUNNING_APP_FOR_REAL = True
 THERMOCYCLER_CONNECTED = False
 TEMPDECK_CONNECTED = False
-CONTROLLER_CONNECTED = False
+CONTROLLER_CONNECTED = True
 
 WINDOWS_SERIAL_PORT_OT2 = "COM4"  # This is the com port generally used by the motors on Windows, but it could be a different number
 LINUX_SERIAL_PORT_OT2 = "/dev/ttyUSB0"  # This is the com port used by the motors on Linux-based operating systems (including Raspberry OS)
